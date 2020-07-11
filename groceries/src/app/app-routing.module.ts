e33f8cd6b23783a2f58296b5cd1c51d45cd447f1
Billie <billiemj@gmail.com>
2020-07-11 11:40:38 -0500

    contact
    replaced tab3 with contact

--- groceries/src/app/app-routing.module.ts
+++ groceries/src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
   },  {
     path: 'about',
     loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
   }
 
 
